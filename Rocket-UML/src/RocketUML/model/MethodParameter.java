94508849558a681dcc3e9e401c8eebd8d9c369de
Dave Weber <rotinom-at-gmail-dot-com>
2014-03-01 22:02:08 -0600

    More updates.  Beginning to flesh out relationship.

---
+++ Rocket-UML/src/RocketUML/model/MethodParameter.java
@@ -0,0 +1,27 @@
+package RocketUML.model;
+
+import RocketUML.ui.Drawable;
+
+/**
+ * Created by rotinom on 3/1/14.
+ */
+public class MethodParameter{
+    private String type;
+    private String name;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- Rocket-UML/src/RocketUML/model/RelationshipElement.java
+++ Rocket-UML/src/RocketUML/model/RelationshipElement.java
@@ -4,10 +4,25 @@ package RocketUML.model;
 import RocketUML.ui.Drawable;
 import RocketUML.visitor.Visitor;
 
+enum RelationshipType{
+  Inheritance,
+
+};
+
 /**
  * Created by rotinom on 2/19/14.
  */
 public class RelationshipElement extends AbstractElement implements Drawable {
+
+    private ClassElement srce; // Source
+    private ClassElement dest; // Destination
+    private ClassElement assc; // Associated class
+
+    private String srceMultiplicity;
+    private String destMultiplicity;
+
+
+
 
     private RelationshipElement() {
     }
