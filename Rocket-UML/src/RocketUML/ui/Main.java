870ea095350e5c35460783b84afef1eda8251c1b
joneason <jonathan.w.eason@gmail.com>
2014-03-11 23:19:08 -0500

    Add Popup menu
    Add popupmenu

--- Rocket-UML/src/RocketUML/ui/Main.java
+++ Rocket-UML/src/RocketUML/ui/Main.java
@@ -1,38 +1,28 @@
 package RocketUML.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 
 public class Main extends JFrame {
 	
 	protected JFrame frame = new JFrame("Rocket UML");
 	protected JPanel theContentPane = new JPanel();
-	
+    protected JPopupMenu popup;
+
 	private Main()
 	{
 		init();
 	}
 
 	private void init() {
-		
-		Menu gMenu = new Menu(this);	
+
+		Menu gMenu = new Menu(this);
 		Toolbar Tool = new Toolbar();
-		
+
         //Create and set up the window.
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) (screenSize.getWidth()/2);
@@ -43,15 +33,42 @@ public class Main extends JFrame {
         Tool.setSize(360,250);
         frame.add(Tool.panel);
         frame.setJMenuBar(gMenu.menuBar);
-
         frame.setLocation(width/2, height/2);
-        
         //  frame.pack();
         frame.setVisible(true);
-        
+
+        popup = new JPopupMenu();
+        // add menu items to popup
+        popup.add(new JMenuItem("Add Class"));
+        popup.add(new JMenuItem("Add Relationship"));
+        popup.addSeparator();
+        popup.add(new JMenuItem("Clear All"));
+        System.out.println("test one two");
+        //Add listener to components that can bring up popup menus.
+        frame.addMouseListener(new MouseListener());
     }
 	
 	public static void main(String[] args) {
 	      Main window = new Main();
 	}
+
+
+    class MouseListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            handleMousePress(e);
+        }
+
+        public void mouseReleased(MouseEvent e) {
+            handleMousePress(e);
+        }
+
+        private void handleMousePress(MouseEvent e) {
+
+            if (e.isPopupTrigger()) {
+                popup.show(e.getComponent(),
+                        e.getX(), e.getY());
+            }
+        }
+    }
+
 }
